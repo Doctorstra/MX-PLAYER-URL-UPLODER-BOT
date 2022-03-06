fa12812a01c95e6970b9740244687ec282fdb761
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-03-06 10:22:35 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -1,1 +1,14 @@
+import pyrogram
 
+from plugins.mxplayer.py import mxplayer_execute
+
+
+@pyrogram.Client.on_callback_query()
+async def formatbuttons(bot, update):
+       
+    if "|" in update.data:
+        await mxplayer_execute(bot, update)
+        
+    elif "closeformat" in update.data:     
+        await update.message.delete()
+    
